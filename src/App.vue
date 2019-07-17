a77e9a9e7a7eb6498aeb10d35e459f1bb92d3986
Airthee <13355624+Airthee@users.noreply.github.com>
2019-07-17 14:26:53 +0200

    Init main vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form-date/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormDate from './components/FormDate.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FormDate
   }
 }
 </script>
---
+++ src/components/FormDate.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div>
+      <input type="text" :v-model="day" placeholder="DD">
+      <input type="text" :v-model="month" placeholder="MM">
+      <input type="text" :v-model="year" placeholder="YYYY">
+      <input type="text" :v-model="hour" placeholder="hh">
+      <input type="text" :v-model="minut" placeholder="mm">
+      <input type="text" :v-model="second" placeholder="ss">
+    </div>
+      
+    <div>
+      <div>Current time : {{ currDate }}</div>
+      <div>Diff time : {{ diffTime }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { setInterval } from 'timers';
+export default {
+  name: 'form-date',
+  data() {
+    return {
+      day: null,
+      month: null,
+      year: null,
+      hour: 0,
+      minut: 0,
+      second: 0,
+      currDate: new Date()
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.currDate = new Date();
+    }, 1000);
+  },
+  computed: {
+    diffTime() {
+      return new Date(
+        this.currDate.getFullYear() - this.year,
+        this.currDate.getMonth() - this.month,
+        this.currDate.getDate() - this.day,
+        this.currDate.getHours() - this.hour,
+        this.currDate.getMinutes() - this.minut,
+        this.currDate.getSeconds() - this.second,
+      );
+    }
+  }
+}
+</script>
+
