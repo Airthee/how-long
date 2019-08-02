2d2ad7c869f78ee7c02fa46ba5b2f6b2557c1778
Airthee <13355624+Airthee@users.noreply.github.com>
2019-08-02 13:59:40 +0200

    Add github link (solved #8)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="app">
-    <h1>How long, what the hell is this ?</h1>
-    <div>
-        <div>You regularly forget your age or the age of a loved one ?</div>
-        <div>Need to know how long you have been with your partner ?</div>
-        <div>Add this website to your favourites and don't think anymore !</div>
-        <div>Just pick a date and see the difference grow instantly !</div>
+    <div class="main-content">
+      <h1>How long, what the hell is this ?</h1>
+      <div>
+          <div>You regularly forget your age or the age of a loved one ?</div>
+          <div>Need to know how long you have been with your partner ?</div>
+          <div>Add this website to your favourites and don't think anymore !</div>
+          <div>Just pick a date and see the difference grow instantly !</div>
+      </div>
+      <form-date/>
     </div>
-    <form-date/>
+    <footer>
+      <div class="github-links">
+        <span class="github-link">
+          <a href="http://github.com/Airthee/how-long" target="_blank">
+            <img height="40px" src="@/assets/GitHub-Mark-Light-120px-plus.png" alt="Github LOGO" title="Access to the Github repository">
+          </a>
+        </span>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -22,22 +33,43 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #cfcfcf;
-  margin-top: 60px;
-}
+<style lang="scss">
+  body {
+    background-color: #2b2b2b;
+    margin: 0;
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
 
-body {
-  background-color: #2b2b2b;
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #cfcfcf;
+      height: 100%;
+      width: 100%;
+    }
+
+    .main-content {
+      position: relative;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      margin: auto;
+    }
+
+    footer {
+      position: absolute;
+      bottom: 0px;
+      width: 100%;
+      background-color: #00000038;
+      color: white;
+
+      .github-links:hover {
+        opacity: 0.3;
+      }
+    }
+  }
 </style>
