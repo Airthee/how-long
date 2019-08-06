9cd8b8a18747d6bf37db82e6c85b80648bf41483
Raphaël TISON <13355624+Airthee@users.noreply.github.com>
2019-08-06 22:44:53 +0200

    add i18n (#16) (close #2)

---
+++ src/i18n-translations.js
@@ -0,0 +1,96 @@
+const i18nTranslation = {
+  translations: {
+    fr: {
+      app: {
+        name: 'How long'
+      },
+      home: {
+        description: {
+          title: "How long, qu'est ce que c'est ?",
+          text: `
+            Il vous arrive d'oublier votre âge ou celui d'un(e) proche ?<br>
+            Besoin de savoir depuis combien de temps dure une relation ?<br>
+            Ajoutez ce site à vos favoris et arrêtez de réfléchir !
+            Renseignez votre date et voyez la différence instantanément !
+          `
+        },
+      },
+      formDate: {
+        elapsedTime: 'Temps écoulé',
+        year: '1 an | {n} ans',
+        month: '{n} mois',
+        day: '1 jour | {n} jours',
+        hour: '1 heure | {n} heures',
+        minute: '1 minute | {n} minutes',
+        second: '1 seconde | {n} secondes',
+        inputTitles: {
+          year: 'Année',
+          month: 'Mois',
+          day: 'Jour',
+          hour: 'Heure',
+          minute: 'Minute',
+          second: 'Seconde'
+        }
+      },
+      footer: {
+        githubTitle: 'Accéder au repo Github'
+      },
+      words: {
+        and: 'et'
+      }
+    },
+    en: {
+      app: {
+        name: 'How long'
+      },
+      home: {
+        description: {
+          title: "How long, what the hell is this ?",
+          text: `
+            You regularly forget your age or the age of a loved one ?<br>
+            Need to know how long you have been with your partner ?<br>
+            Add this website to your favourites and don't think anymore !<br>
+            Just pick a date and see the difference grow instantly !<br>
+          `
+        },
+      },
+      formDate: {
+        elapsedTime: 'Elapsed time',
+        year: '1 year | {n} years',
+        month: 'month | {n} months',
+        day: '1 day | {n} days',
+        hour: '1 hour | {n} hours',
+        minute: '1 minute | {n} minutes',
+        second: '1 second | {n} seconds',
+        inputTitles: {
+          year: 'Year',
+          month: 'Month',
+          day: 'Day',
+          hour: 'Hour',
+          minute: 'Minute',
+          second: 'Second'
+        }
+      },
+      footer: {
+        githubTitle: 'Access to the Github repository'
+      },
+      words: {
+        and: 'and'
+      }
+    },
+  },
+
+  getCurrentLang() {
+    return localStorage.lang || (window.navigator.userLanguage || window.navigator.language).split('-')[0];
+  },
+
+  setLang(lang) {
+    localStorage.lang = lang;
+    window.location.reload();
+  }
+};
+
+// update html element
+document.querySelector('html').setAttribute('lang', i18nTranslation.getCurrentLang());
+
+export default i18nTranslation;
