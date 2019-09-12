bbd6b334c50e3b8e9d99f0ebf37f1b87b4672a9a
Airthee <13355624+Airthee@users.noreply.github.com>
2019-09-12 20:35:37 +0200

    Add alt to logo image

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="content">
-      <img class="home-logo" src="/logo.svg">
+      <img class="home-logo" src="/logo.svg" alt="How Long logo">
       <h1>{{ $t('home.description.title') }}</h1>
       <p v-html="$t('home.description.text')"></p>
       <form-date/>
