ac8a29fa5bdfe1f14858930477c89fb1b505ff49
Lucas Gomes <lucasgomesp22@gmail.com>
2020-10-30 14:14:37 -0300

    move sharing buttons to footer

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,19 +6,16 @@
       <p v-html="$t('home.description.text')"></p>
       <form-date />
     </div>
-    <social-sharing-buttons />
   </div>
 </template>
 
 <script>
 import FormDate from "./FormDate";
-import SocialSharingButtons from "./SocialSharingButtons";
 
 export default {
   name: "home",
   components: {
-    FormDate,
-    SocialSharingButtons,
+    FormDate
   },
 };
 </script>
